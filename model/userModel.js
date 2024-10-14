01f3c641b342b2a8dcdde23abe999ea14d15f429
PreethiSundaramoorthi-25071999 <preethisundhar.ps@gmail.com>
2024-10-14 23:24:43 +0530

    new

---
+++ model/userModel.js
@@ -0,0 +1,28 @@
+
+// Import the mongoose 
+const mongoose = require("mongoose");
+
+// define the schema
+const userSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+
+    password: { 
+        type: String, 
+        required: true 
+    },
+})
+
+// create the user modele
+const User = mongoose.model("User", userSchema)
+
+module.exports = User;
